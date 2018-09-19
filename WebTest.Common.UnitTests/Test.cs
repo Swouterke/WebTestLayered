483b76c651ba4522703c94abf1bb3f2dbe8f03a2
LoQutwouterus NV <wvercruysse45@gmai.comLoQutus@live.com>
2018-09-19 10:31:35 +0200

    added UnitTest for Person-class

---
+++ WebTest.Common.UnitTests/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebTest.Model;
+
+namespace WebTest.Common.UnitTests
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void PersonTest()
+        {
+            Person person = new Person(4, "jeanke", 3);
+            bool isPerson = true;
+            if (!person.GetType().Equals(typeof(Person))) 
+            {
+                isPerson = false;
+            }
+            Assert.IsTrue(isPerson);
+        }
+    }
+}
