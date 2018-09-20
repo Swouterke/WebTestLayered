64028c2593f61c58523b63a22276f3142a2bfaf5
LoQutwouterus NV <wvercruysse45@gmai.comLoQutus@live.com>
2018-09-20 14:51:21 +0200

    added migration initialcreate

---
+++ WebTest.Model/Migrations/201809201221299_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace WebTest.Model.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Person",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(),
+                        Number = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Person");
+        }
+    }
+}
